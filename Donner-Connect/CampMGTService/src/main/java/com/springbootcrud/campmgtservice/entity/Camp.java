504271b2115fd2bc25f8faba07c4b4f72e436c4e
Nuthara <nutharayuthmini@gmail.com>
2024-08-19 18:45:51 +0530

    Camp service updated

--- Donner-Connect/CampMGTService/src/main/java/com/springbootcrud/campmgtservice/entity/Camp.java
+++ Donner-Connect/CampMGTService/src/main/java/com/springbootcrud/campmgtservice/entity/Camp.java
@@ -1,4 +1,25 @@
 package com.springbootcrud.campmgtservice.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.sql.Time;
+import java.util.Date;
+
+@Entity
 public class Camp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String location;
+    private Date date;
+    private Time time;
+    private String status;
+
+
 }
