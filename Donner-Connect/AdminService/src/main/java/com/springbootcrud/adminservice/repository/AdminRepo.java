df0092d1cf70f1d51fec8baf2d569dd1f1d2a9f2
MokshaDill <mokshadil@gmail.com>
2024-08-22 08:01:26 +0530

    deletes

--- Donner-Connect/AdminService/src/main/java/com/springbootcrud/adminservice/repository/AdminRepo.java
+++
@@ -1,7 +0,0 @@
-package com.springbootcrud.adminservice.repository;
-
-import com.springbootcrud.adminservice.entity.Admin;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AdminRepo extends CrudRepository<Admin, Integer> {
-}
