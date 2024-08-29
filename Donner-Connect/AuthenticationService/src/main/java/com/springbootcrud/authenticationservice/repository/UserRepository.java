436ae5e8e056acafc88a8f387ce8fde1dec240ed
Naveen Silva <95732242+NaveenSilva@users.noreply.github.com>
2024-08-30 02:17:00 +0530

    authentication service

---
+++ Donner-Connect/AuthenticationService/src/main/java/com/springbootcrud/authenticationservice/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.springbootcrud.authenticationservice.repository;
+
+import com.springbootcrud.authenticationservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+
+}
