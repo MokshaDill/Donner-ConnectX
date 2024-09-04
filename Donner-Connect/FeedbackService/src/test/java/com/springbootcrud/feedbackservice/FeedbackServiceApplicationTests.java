782b316e7f96f04694d86cc9da1214064c5e83e9
Sadeep Amantha De Silva <sadeepamantha00@gmail.com>
2024-09-04 22:33:07 +0530

    Add Feedback section (#10)
    Add Feedback section

--- Donner-Connect/FeedbackService/src/test/java/com/springbootcrud/feedbackservice/FeedbackServiceApplicationTests.java
+++ Donner-Connect/FeedbackService/src/test/java/com/springbootcrud/feedbackservice/FeedbackServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.springbootcrud.analysisservice;
+package com.springbootcrud.feedbackservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AnalysisServiceApplicationTests {
+class FeedbackServiceApplicationTests {
 
     @Test
     void contextLoads() {
