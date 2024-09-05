c651ba7608b5885d1f3d9d19d698bc324fe87466
KavindiViranga <97431820+KavindiViranga@users.noreply.github.com>
2024-09-05 17:20:27 +0530

    Kavindi user service (#11)
    * commit
    
    * added UserService backend
    
    * add 'user register as a donor'
    
    * add 'view, update and delete operations'
    
    * add 'view donation history'

---
+++ Donner-Connect/UserService/src/main/java/com/springbootcrud/userservice/service/UserService.java
@@ -0,0 +1,16 @@
+package com.springbootcrud.userservice.Service;
+
+import com.springbootcrud.userservice.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    User registerUser(User user);
+    User updateUser(Long id, User user);
+    User getUserById(Long id);
+    void deleteUser(Long id);
+    List<String> getDonationHistory(Long userId);
+
+    List<User> getAllUsers();
+
+}
